ea9fbdef243117960dbadb8709b259dc032b7b3e
qVictor <qqlogin@yandex.ru>
2024-07-04 15:17:56 +0300

    required validator can work with number

--- src/Validators/RequiredValidator.php
+++ src/Validators/RequiredValidator.php
@@ -4,8 +4,12 @@ namespace App\Validators;
 
 class RequiredValidator implements ValidatorInterface
 {
-    public function isValid($value): bool
+    public function isValid($value, $type = Validator::TYPE_VALIDATION_STRING): bool
     {
-        return !empty($value);
+        if ($type == Validator::TYPE_VALIDATION_STRING) {
+            return !empty($value);
+        } else {
+            return is_int(value: $value);
+        }
     }
 }
